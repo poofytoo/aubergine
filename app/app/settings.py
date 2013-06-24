d994703b97998992ea22950d0cb4a988bba0a1e7
Apple Store <applestore@Evas-MacBook-Pro.local>
2013-06-23 23:03:16 -0400

    reconfigured for development database

--- app/app/settings.py
+++ app/app/settings.py
@@ -12,10 +12,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db-aubergine',                      # Or path to database file if using sqlite3.
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'NAME': 'vhung+aubergine',                      # Or path to database file if using sqlite3.
+        'USER': 'vhung',
+        'PASSWORD': 'pencilpencil',
+        'HOST': 'sql.mit.edu',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
 }
