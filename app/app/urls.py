283ce4411948a7c019f9c3c081bf4e5b70cf5e9a
Stephanie Yu <styu314@gmail.com>
2013-06-26 00:47:01 -0700

    Downloaded custom build of foundation with only grid/visibility css. Set up django to serve static files. Currently serving for deployment (locally) only

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,3 +14,16 @@ urlpatterns += patterns('django.contrib.flatpages.views',
 
 )
 
+# REMOVE ALL LINES BELOW WHEN WE DEPLOY TO PRODUCTION SERVER
+# IT IS GROSSLY INEFFICIENT
+# FOR DEPLOYMENT ONLY!!
+from django.conf import settings
+
+if settings.DEBUG:
+    urlpatterns += patterns('django.contrib.staticfiles.views',
+        url(r'^static/(?P<path>.*)$', 'serve'),
+    )
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns += staticfiles_urlpatterns()
